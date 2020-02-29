e3f4988dcbc963a0909c3ddaad56dae2606bb6fe
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-03-01 02:19:26 +0700

    sangat commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import matematika as M
+
+M.Tambah(98, 44)
+M.Kurang(76, 21)
+
---
+++ matematika.py
@@ -0,0 +1,13 @@
+def Tambah(a, b):
+    print('a', '+', 'b', '=', a, '+', b, '=', a + b)
+
+def Kurang(a, b):
+    print('a', '-', 'b', '=', a, '-', b, '=', a - b)
+
+print(__name__)
+
+def main():
+    print('Ini adalah fungsi utama dari Matematika')
+
+if __name__ == '__main__':
+    main()
